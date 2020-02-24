3c98d2cc2867a8ff426ae6fe23c8052c2dfa7d8d
BAH <a>
2020-02-24 16:35:15 +0100

    base de donnée

---
+++ Bomberman-JEE/src/dao/bdd/DaoFactory.java
@@ -0,0 +1,41 @@
+package dao.bdd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DaoFactory {
+    private String url;
+    private String username;
+    private String password;
+    
+    
+    DaoFactory(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    public static DaoFactory getInstance() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+
+        }
+
+        DaoFactory instance = new DaoFactory(
+                "jdbc:mysql://localhost:3306/javaee", "root", "Dalabacity93");
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    
+    // Récupération du Dao
+    public UtilisateurDAO getUtilisateurDao() {
+        return new UtilisateurDaoImp(this); 
+    }
+    
+}
